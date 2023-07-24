131f145fe3cf8604e1c299a958a83037cd647da9
Adeel Ahmed <helloadeelahmed@gmail.com>
2023-07-24 12:04:46 +0500

    Fixed the data type

--- Day 2/sum-of-two-digits.py
+++ Day 2/sum-of-two-digits.py
@@ -3,6 +3,6 @@ number = input("Enter a 2 digits number: ")
 first_digit = number[0]
 second_digit = number[1]
 
-total = first_digit + second_digit
+total = int(first_digit) + int(second_digit)
 
-print("The sum of those 2 digits is: " + total)
+print("The sum of those 2 digits is: " + str(total))
